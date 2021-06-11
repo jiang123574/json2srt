9a60fcb8a6c9b29a079d2c05a1e7d0de1b950600
clegea <jiang123574@163.com>
2021-06-11 15:12:03 +0800

    修复bug

--- json2srt/settings.py
+++ json2srt/settings.py
@@ -123,6 +123,5 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [("css", os.path.join(STATIC_ROOT, 'css')), ("imge", os.path.join(STATIC_ROOT, 'imge'))]
